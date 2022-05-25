2de6b7e15798f3ebbaf69ccbc9909738cbdce7fc
Paul <paulmichaelhardman@gmail.com>
2022-05-25 09:51:41 +0100

    all

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import { ChannelContainer, ChannelListContainer } from "./Componenets";
+
+const apiKey = "5mzxmxmjryf5";
+const client = StreamChat.getInstance(apiKey);
+
+function App() {
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+}
+
+export default App;
